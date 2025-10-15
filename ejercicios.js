f2e703099c418659048010d9cb35db4eabc89142
sergio martinez padilla <sermarpad@gmail.com>
2025-10-15 13:54:20 +0200

    aaaaaaaaaaaa

--- ejercicios.js
+++ ejercicios.js
@@ -214,6 +214,7 @@ const personas = [persona1, persona2, persona3];
 console.log(personas)
 for (let i=0; i < personas.length;i++){
     console.log(personas[i], "hola")
+    console.log(personas[i].nombre, "nombre")
 }
 //9
 const colores = ["rojo", "azul", "verde"];
