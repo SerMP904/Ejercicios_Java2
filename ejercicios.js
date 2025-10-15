08990e4e7af629c12a1c1137138f6f0e3ec88822
sergio martinez padilla <sermarpad@gmail.com>
2025-10-15 10:26:06 +0200

    commit 1

--- ejercicios.js
+++ ejercicios.js
@@ -51,9 +51,65 @@ console.log(cola);
 
 //7
 const items = ["altura", "base", "cuadrado", "bisectriz"];
+let found = "false";
 items.forEach(item =>{
+    if (found === "true") return;
     console.log(item);
     console.log("Indice:" + items.indexOf(item));
+    for (const b of item){
+        if (b === "b"){
+        console.log("En", item, "hay una b en la posición", item.indexOf(b));
+        found = "true";
+        break;
+    }
+    }
 });
 
 //8
+let letras = ["a", "b", "c", "d"];
+let primero = letras.slice(0, 2);
+let ultimo = letras.slice((letras.length-3), (letras.length));
+console.log(primero);
+console.log(ultimo);
+//9
+let primnum = [1, 2];
+let segnum = [3, 4];
+
+let nums = primnum.concat(segnum);
+
+console.log(nums);
+
+//10
+const números = [2, 5, 8, 12];
+const morethanten = números.some(num => num > 10);
+const morethanzero = números.every(num => num > 0);
+const haseight = números.includes(8);
+
+console.log(morethanten, morethanzero, haseight);
+
+//11
+const cod = [7, 8, 9];
+let cod2 = cod.slice();
+console.log(cod2.reverse());
+console.log(cod.join("-"))
+
+//12
+const base = [1, 2, 3];
+let cuadrados = [] 
+base.forEach((item, index)=>{
+    cuadrados[index] = item**2;
+})
+console.log(cuadrados)
+//13
+const pagos = [10, 20, 5];
+let total = 0;
+for (i = 0; i < pagos.length; i++){
+    total = total + pagos[i];
+}
+console.log(total);
+//14
+let llamadadesorden = [10, 2, 1, 20];
+llamadadesorden = llamadadesorden.sort((a, b) => a-b)
+console.log(llamadadesorden);
+llamadadesorden = llamadadesorden.sort((a, b) => b-a)
+console.log(llamadadesorden);
